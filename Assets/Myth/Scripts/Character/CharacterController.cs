8c67aaff63d2099aa63a1ac7d8f63a2227faabfe
unknown <xerosgame@hotmail.com>
2022-11-09 15:20:27 -0700

    Added new Character and Icon assets

--- Assets/Myth/Scripts/Character/CharacterController.cs
+++ Assets/Myth/Scripts/Character/CharacterController.cs
@@ -3,18 +3,23 @@ using UnityEngine.InputSystem;
 
 namespace Com.Myth.SlashBuild
 {
+    [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(CharacterMotor))]
     public class CharacterController : MonoBehaviour
     {
         public float movementSpeed;
 
+        private Animator animator;
         private CharacterMotor motor;
+        private SpriteRenderer spriteRenderer;
 
         private Vector2 m_MoveDirection;
 
         public void Start()
         {
+            animator = GetComponent<Animator>();
             motor = GetComponent<CharacterMotor>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         public void Update()
@@ -29,7 +34,24 @@ namespace Com.Myth.SlashBuild
 
         private void MovePlayer()
         {
-            motor.Move(m_MoveDirection * movementSpeed * Time.deltaTime);
+            if (m_MoveDirection != Vector2.zero)
+            {
+                motor.Move(m_MoveDirection * movementSpeed * Time.deltaTime);
+                animator.SetBool("Walking", true);
+            }
+            else
+            {
+                animator.SetBool("Walking", false);
+            }
+
+            if (m_MoveDirection.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
         }
     }
 }
